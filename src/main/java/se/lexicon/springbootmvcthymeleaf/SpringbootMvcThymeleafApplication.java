282cef560d4f774ef7e04b90b47ca3646e02ee98
Leo Yoosefi <101973222+leoyoosefi@users.noreply.github.com>
2022-07-04 12:23:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/springbootmvcthymeleaf/SpringbootMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootmvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMvcThymeleafApplication.class, args);
+    }
+
+}
