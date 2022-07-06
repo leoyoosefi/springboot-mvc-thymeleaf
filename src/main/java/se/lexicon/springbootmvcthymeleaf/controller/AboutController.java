5728fadd968fb02ee5f5eb00a79564873cefb5d0
Leo Yoosefi <101973222+leoyoosefi@users.noreply.github.com>
2022-07-06 18:38:37 +0200

    add test message

--- src/main/java/se/lexicon/springbootmvcthymeleaf/controller/AboutController.java
+++ src/main/java/se/lexicon/springbootmvcthymeleaf/controller/AboutController.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 public class AboutController {
     @GetMapping("/about")
     public String about(Model model){
+        System.out.println("test");
         LocalDate currentDate = LocalDate.now();
         model.addAttribute("serverDate", currentDate);
         return "about";
